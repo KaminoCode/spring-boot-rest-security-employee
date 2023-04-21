2848ba515a2c698de63f04f19bd4d49dfbdfb779
ozark-dev <jason@ozarkdev.com>
2023-04-20 22:56:43 -0500

    Adding spring security to pom.xml

--- src/main/java/com/kaminocode/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/kaminocode/cruddemo/dao/EmployeeRepository.java
@@ -2,9 +2,8 @@ package com.kaminocode.cruddemo.dao;
 
 import com.kaminocode.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path="members")
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // that's it...no need to write any code
